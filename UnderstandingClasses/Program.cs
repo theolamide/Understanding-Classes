66822a42a8b03cb62f4d8b8661c8abc3c0871e1a
theolamide <ajadidan@gmail.com>
2020-10-12 23:39:57 -0400

    Add project files.

---
+++ UnderstandingClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace UnderstandingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "OldsMobile";
+            myCar.Model = "Cutlass Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+            Console.ReadLine(); 
+        }
+        
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+                carValue = 159000; //This is hard coded and \
+            else
+                carValue = 2000;
+
+            return carValue;
+        }
+
+    }
+}
